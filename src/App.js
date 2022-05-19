5c160bbe49f03f97746ee32aa1cbbc7bc92358d1
klari <klaritatis@gmail.com>
2022-05-19 00:35:18 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import ResultContainer from "./components/ResultContainer";
+import Home from "./components/Home";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home/> }>
+          <Route path="vagas" element={<ResultContainer />} />
+          {/*<Route path="cadastro" element={<Form />} />*/}
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
